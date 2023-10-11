c6251fb75b370b41eaf35f0aac9c0f6f99349e14
anicloud789 <kumar.anand.cloud2@gmail.com>
2023-10-11 15:56:40 +0530

    mvv format

--- ContactCRMTriggerDEL/index.js
+++ ContactCRMTriggerDEL/index.js
@@ -1,3 +1,5 @@
-module.exports = async function(context, mySbMsg) {
-    context.log('JavaScript ServiceBus queue trigger function processed message', mySbMsg);
-};
+// module.exports = async function(context, mySbMsg) {
+//     context.log('JavaScript ServiceBus queue trigger function processed message', mySbMsg);
+// };
+
+module.exports = require('./controllers/contactController');
