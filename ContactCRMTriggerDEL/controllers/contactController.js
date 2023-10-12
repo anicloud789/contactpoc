c62c5098ef25839726680e1dba98067735ce88f8
anicloud789 <kumar.anand.cloud2@gmail.com>
2023-10-12 19:41:38 +0530

    curd update and delete 1 operation

--- ContactCRMTriggerDEL/controllers/contactController.js
+++ ContactCRMTriggerDEL/controllers/contactController.js
@@ -4,64 +4,64 @@ const customErrorHandler = require('../errorHandling/customErrorHandler');
 module.exports = async function (context, req) {
     try {
 
-        context.log("req=>",req);
-        
-        const action = req.action; // Assume action is 'get', 'create', 'update', or 'delete'
-        const contactId = req.contactId; // Assuming the contact ID is passed
+        context.log("req=>", req);
 
-        context.log("action=>",action);
-        
+        const action = req.action;
+        const contactId = req.contactId;
+
+        context.log("action=>", action);
+
         if (action === 'getAll') {
             const contact = await salesforceService.getAllContact();
 
-            context.log("ALL contacts=>",JSON.stringify(contact.recentItems,null,2));
+            context.log("ALL contacts=>", JSON.stringify(contact.recentItems, null, 2));
             context.res = {
                 status: 200,
                 body: contact.recentItems
             };
 
 
-        }else if (action === 'create') {
-                // Implement create logic
-                const contactDetails = req.data; // Assuming the contact details are passed in the request body
-                const createdContact = await salesforceService.createContact(contactDetails);
-                context.res = {
-                    status: 201,
-                    body: createdContact
-                };
-            }
-        //else if (action === 'get') {
-        //     const contact = await salesforceService.getContact(contactId);
-        //     context.res = {
-        //         status: 200,
-        //         body: contact
-        //     };
-        // } else if (action === 'update') {
-        //     // Implement update logic
-        //     const contactDetails = req.body; // Assuming the contact details are passed in the request body
-        //     const updatedContact = await salesforceService.updateContact(contactId, contactDetails);
-        //     context.res = {
-        //         status: 200,
-        //         body: updatedContact
-        //     };
-        // } else if (action === 'delete') {
-        //     // Implement delete logic
-        //     await salesforceService.deleteContact(contactId);
-        //     context.res = {
-        //         status: 204
-        //     };
-        // } 
+        } else if (action === 'create') {
+            // Implement create logic
+            const contactDetails = req.data;
+            const createdContact = await salesforceService.createContact(contactDetails);
+            context.res = {
+                status: 201,
+                body: createdContact
+            };
+        }
+        else if (action === 'update') {
+            const contactDetails = req.data;
+            const updatedContact = await salesforceService.updateContact(contactId, contactDetails);
+            context.res = {
+                status: 200,
+                body: updatedContact
+            };
+        }
+        else if (action === 'delete') {
+            await salesforceService.deleteContact(contactId);
+            context.res = {
+                status: 204
+            };
+        }
+        else if (action === 'get') {
+            const contact = await salesforceService.getContact(contactId);
+            context.res = {
+                status: 200,
+                body: contact
+            };
+        }
         else {
-            customErrorHandler({message:"Invalid action specified"}, context);
+            customErrorHandler({ message: "Invalid action specified" }, context);
             context.res = {
                 status: 200,
                 body: 'Invalid action specified.'
             };
-            
-         
+
+
         }
     } catch (error) {
-       // context.log("error=>",error);
+        // context.log("error=>",error);
         // Handle errors using custom error handler and logging
         customErrorHandler(error, context);
     }
